6112ea34c5bf635897a2242613ce9f6227772444
Erick Ramirez <erickmi01x@gmail.com>
2026-05-27 09:03:24 -0600

    feat: implement event bus signal system

--- autoload/EventBus.gd
+++ autoload/EventBus.gd
@@ -1,11 +1,17 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#core gameplay events
 
+signal player_died
+signal score_updated(score)
+signal bioma_changed(bioma_name)
+signal item_collected(item_id)
+signal checkpoint_reached(checkpoint_id)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#feature extensibility
+
+signal game_started
+signal game_paused
+signal game_resumed
+signal game_over
